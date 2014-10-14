f0e2981ec6037fbd49310afca31b4d83b473660b
Tin Ho <tintho@uw.edu>
2014-10-14 12:09:54 -0700

    learned about javascriptm event listener, dom content

---
+++ js/app.js
@@ -0,0 +1,40 @@
+/* this is our application code */
+"use strict";
+
+var name = 'Tin';
+
+var fruits = ['apple', 'banana', 'pear', 'kiwi'];
+fruits.push('mango');
+
+for(var idx = 0; idx < fruits.length; ++idx) {
+    console.log(fruits[idx]);
+}
+
+function sayHello(name) {
+    alert('Hello ' + name);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elem = document.getElementById('message');
+    elem.innerHTML = 'Hello ' + name;
+
+    var nameInput = document.getElementById('name-input');
+    nameInput.value = name;
+    nameInput.addEventListener('keyup', function() {
+        elem.innerHTML = 'Hello ' + nameInput.value;
+    });
+
+    var fruitsList = document.getElementById('fruits-list');
+    fruits.forEach(function(fruit) {
+        var fruitLi = document.createElement('li');
+        fruitLi.innerHTML = fruit;
+        fruitsList.appendChild(fruitLi);
+    });
+
+    var animalPic= document.getElementById('animal-pic');
+    animalPic.addEventListener('click', function() {
+        var animalSound = document.getElementById('animal-sound');
+        animalSound.play();
+    });
+});
+
